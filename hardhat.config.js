ae88c59e1e2ef3a067017e20ff198dd88dfceebe
shared <sharadpoddar1001@gmail.com>
2024-01-04 00:04:22 +0530

    setting up hardhat config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: {
+    compilers: [{value: "0.8.19"}, {value: "0.8.20"}],
+  },
+  networks: {
+    mumbai:{
+      url: process.env.RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    }
+  }
 };
